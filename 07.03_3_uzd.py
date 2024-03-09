42eed17951ef2d2b1b023dca5d2e33e9b54b5a2c
LKgame <71346849+LKgame35@users.noreply.github.com>
2024-03-09 12:30:19 +0200

    Update 07.03_3_uzd.py

--- 07.03_3_uzd.py
+++ 07.03_3_uzd.py
@@ -1,82 +1,133 @@
+import threading
+import time
 import tkinter as tk
 from time import strftime
 import sqlite3
 
-koef = 0
-
-savienojums = sqlite3.connect("laiks.db")
-kursors = savienojums.cursor()
-
-class Pulkstenis:
-
-    def __init__(self, stundas, minutes,sekundes):
-
-        self.stundas=stundas
-        self.minutes=minutes
-        self.sekundes=sekundes
-
-        kursors.execute('''CREATE TABLE IF NOT EXISTS laiks (
-                    id INTEGER PRIMARY KEY,
-                    stundas INT NOT NULL,
-                    minutes INT NOT NULL,
-                    sekundes INT NOT NULL)
-                    ''')
-
-    def ieslegts(self, stundas, minutes, sekundes):
-
-        kursors.execute(f'''INSERT INTO visitors (stundas, minutes, sekundes)
-            VALUES("{stundas}", "{minutes}", "{sekundes}",0)
-            ''')
-
-
-
-
-def stunda():
-    koef=1
-def minute():
-    koef=1.08
-def sekunde():
-    koef=0.86
-
-def time():
-    tt = strftime('%H:%M:%S %p')
-    a.config(text=tt)
-    a.after(1000, time)
-
-s=Pulkstenis(14,12,56)
-
-def pievienot():
-
-    sekundes = e_skaitlis1.get()
-    minutes = e_skaitlis2.get()
-    stundas = e_skaitlis3.get()
-
-    nn= strftime(f'{stundas}H:{minutes}M:{sekundes}S %p')
-
-def iestatit():
-    sekundes = e_skaitlis1.get()
-    minutes = e_skaitlis2.get()
-    stundas = e_skaitlis3.get()
-
-
-
+stundas = 0
+minutes=0
+sekundes=0
+eror_teksts=""
 
 root = tk.Tk()
-
 root.title("Laiks")
 root.geometry("500x400")
 root.resizable(0,0)
 
-l_virsraksts = tk.Label(root, text='Stundas', font=(25), fg="red")
+
+savienojums = sqlite3.connect("laiks2.db")
+kursors = savienojums.cursor()
+
+class Pulkstenis:
+
+    def __init__(self, stundas2, minutes2,sekundes2):
+
+        self.stundas=stundas2
+        self.minutes=minutes2
+        self.sekundes=sekundes2
+
+        kursors.execute('''CREATE TABLE IF NOT EXISTS laiks2 (
+                    id INTEGER PRIMARY KEY,
+                    stundas2 INT NOT NULL,
+                    minutes2 INT NOT NULL,
+                    sekundes2 INT NOT NULL)
+                    ''')
+
+    def iestatija(self, stundas2, minutes2, sekundes2):
+
+        kursors.execute(f'''INSERT INTO laiks2 (stundas2, minutes2, sekundes2)
+            VALUES("{stundas2}", "{minutes2}", "{sekundes2}")
+            ''')
+
+def plus_minute():
+    global minutes
+    global sekundes
+    minutes+=1
+    sekundes=0
+
+def plus_stunda():
+    global minutes
+    global stundas
+    stundas+=1
+    minutes=0
+
+
+def increment_variable():
+    global stundas
+    global sekundes
+    global minutes
+
+    while True:
+        if sekundes==59:
+            plus_minute()
+        else:    
+            sekundes += 1
+        if minutes==59:
+            plus_stunda()
+        
+        a = tk.Label(root, text=f'{stundas}:{minutes}:{sekundes}',font=('calibri', 25), fg="green")
+        a.place(x=140, y=300)
+
+        l_virsraksts = tk.Label(root, text=f'{eror_teksts}', font=(25), fg="red")
+        l_virsraksts.place(x=220, y=250)
+
+        time.sleep(1)
+
+
+
+def iestatit():
+    global stundas
+    global sekundes
+    global minutes
+    global eror_teksts
+
+    stundas1 = int(e_skaitlis1.get())
+    minutes1 = int(e_skaitlis2.get())
+    sekundes1 = int(e_skaitlis3.get())-1
+
+    if stundas1>23 or minutes1>59 or sekundes1>59:
+        eror_teksts="Neiespējams laiks ievadīts"
+    else:
+        n=Pulkstenis(stundas1,minutes1,sekundes1)
+        n.iestatija(stundas1,minutes1,sekundes1)
+        stundas=stundas1
+        minutes=minutes1
+        sekundes=sekundes1
+
+
+def pievienot():
+    global stundas
+    global sekundes
+    global minutes
+    global eror_teksts
+
+    stundas1 = int(e_skaitlis1.get())
+    minutes1 = int(e_skaitlis2.get())
+    sekundes1 = int(e_skaitlis3.get())
+
+    if stundas+stundas1>23 or minutes1+minutes>59 or sekundes1+sekundes>59:
+        eror_teksts="Neiespējams laiks ievadīts"
+    else:
+        stundas+=stundas1
+        minutes+=minutes1
+        sekundes+=sekundes1
+
+
+
+increment_thread = threading.Thread(target=increment_variable)
+increment_thread.start()
+
+
+
+
+l_virsraksts = tk.Label(root, text='Stundas', font=(25), fg="black")
 l_virsraksts.place(x=80, y=70)
 
-l_virsraksts = tk.Label(root, text='Minūtes', font=(25), fg="red")
+l_virsraksts = tk.Label(root, text='Minūtes', font=(25), fg="black")
 l_virsraksts.place(x=220, y=70)
 
-l_virsraksts = tk.Label(root, text='Sekundes', font=(25), fg="red")
+l_virsraksts = tk.Label(root, text='Sekundes', font=(25), fg="black")
 l_virsraksts.place(x=370, y=70)
-
-
 
 b_poga = tk.Button(root, text="Pievienot", command=pievienot)
 b_poga.place(x=150, y=150)
@@ -93,22 +144,10 @@ e_skaitlis2.place(x=200, y=100)
 e_skaitlis3 = tk.Entry(root)
 e_skaitlis3.place(x=350, y=100)
 
-
-jaunais = tk.Label(root, text='Laiks: ', font=(25), fg="red")
-jaunais.place(x=180, y=200)
-
-
-
-
-    
-a = tk.Label(root, font=('calibri', 25), fg="red")
-a.place(x=140, y=300)
+jaunais = tk.Label(root, text='Laiks: ', font=(25), fg="black")
+jaunais.place(x=140, y=280)
 
 
 
 
-# l_virsraksts = tk.Label(root, text=f'Laiks{a}', font=(25), fg="red")
-# l_virsraksts.place(x=220, y=300)
-
-time()
 root.mainloop()
